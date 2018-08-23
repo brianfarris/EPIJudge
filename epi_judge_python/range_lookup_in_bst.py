24ecf523ce30f77a361dae1f24b1503c51fc1edf
bfarris8 <bfarris8@bloomberg.net>
2018-08-23 09:11:00 -0400

    work

--- epi_judge_python/range_lookup_in_bst.py
+++ epi_judge_python/range_lookup_in_bst.py
@@ -7,7 +7,23 @@ Interval = collections.namedtuple('Interval', ('left', 'right'))
 
 def range_lookup_in_bst(tree, interval):
     # TODO - you fill in here.
-    return []
+    output = []
+    def rec(tree, interval):
+        if tree is None:
+            return
+        if tree.data >= interval.left and tree.data <= interval.right:
+            rec(tree.left, interval)
+            output.append(tree.data)
+            rec(tree.right, interval)
+        elif tree.data > interval.left:
+            rec(tree.left, interval)
+        elif tree.data < interval.right:
+            rec(tree.right, interval)
+    
+    rec(tree, interval)
+    return output
+
+
 
 
 def range_lookup_in_bst_wrapper(tree, i):
