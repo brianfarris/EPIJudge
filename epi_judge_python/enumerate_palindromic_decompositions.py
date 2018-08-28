61eb5e4b17e8121a67eadcc77cd4bafd524ce7fc
Brian Farris <briandavidfarris@gmail.com>
2018-08-27 20:45:33 -0400

    save

--- epi_judge_python/enumerate_palindromic_decompositions.py
+++ epi_judge_python/enumerate_palindromic_decompositions.py
@@ -1,10 +1,19 @@
 from test_framework import generic_test
-
+import sys
 
 def palindrome_decompositions(input):
     # TODO - you fill in here.
-    return []
-
+    #print("input: ", input)
+    if input == '':
+        return [[]]
+    output = []
+    for i in range(1,len(input)+1):
+        prefix = input[:i]
+        if prefix == prefix[::-1]:
+            pd = palindrome_decompositions(input[i:])
+            output += [[prefix] + x for x in pd]
+        i += 1
+    return output
 
 def comp(a, b):
     return sorted(a) == sorted(b)
