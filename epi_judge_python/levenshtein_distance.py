bc1092f83bd1097b13d170e32e5c2093f7524c29
Brian Farris <bfarris@SteinAdmins-MacBook-Pro.local>
2018-09-01 16:11:03 -0400

    save

--- epi_judge_python/levenshtein_distance.py
+++ epi_judge_python/levenshtein_distance.py
@@ -1,9 +1,25 @@
 from test_framework import generic_test
 
-
 def levenshtein_distance(A, B):
+    cache = {}
+
     # TODO - you fill in here.
-    return 0
+    def rec(Ai, Bi):
+        if Ai < 0:
+            return Bi + 1
+        elif Bi < 0:
+            return Ai + 1
+        if (Ai, Bi) not in cache:
+            if A[Ai] == B[Bi]:
+                cache[(Ai, Bi)] = rec(Ai - 1, Bi - 1)
+            else:
+                temp1 = rec(Ai - 1, Bi)
+                temp2 = rec(Ai, Bi - 1)
+                temp3 = rec(Ai - 1, Bi - 1)
+                temp = min(temp1, temp2, temp3) +  1
+                cache[(Ai, Bi)] = temp
+        return cache[(Ai, Bi)]
+    return rec(len(A)-1, len(B)-1)
 
 
 if __name__ == '__main__':
--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -1,11 +1,24 @@
 from test_framework import generic_test
+import copy
+
 
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
     # TODO - you fill in here.
-    return 0
+    storage = {}
+    for x in range(len(individual_play_scores)+1):
+        storage[(x, 0)] = 1
+    for i in range(1, final_score + 1):
+        storage[(0, i)] = 0
+    for use_index in range(0, len(individual_play_scores)):
+        for this_final_score in range(1, final_score + 1):
+            num_uses = 0
+            storage[(use_index+1, this_final_score)] = 0
+            for num_uses in range( this_final_score // individual_play_scores[use_index] + 1):
+                storage[(use_index+1, this_final_score)] += storage[(use_index,
+                                                                  this_final_score - num_uses * individual_play_scores[use_index])]
 
-
+    return storage[(len(individual_play_scores), final_score)]
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("number_of_score_combinations.py",
