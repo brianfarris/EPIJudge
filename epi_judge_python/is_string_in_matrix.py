211bdea224e969368e75def858ef4f5e6ba2615c
Brian Farris <briandavidfarris@gmail.com>
2018-09-01 22:17:42 -0400

    save

--- epi_judge_python/is_string_in_matrix.py
+++ epi_judge_python/is_string_in_matrix.py
@@ -3,8 +3,26 @@ from test_framework import generic_test
 
 def is_pattern_contained_in_grid(grid, S):
     # TODO - you fill in here.
-    return True
+    m = len(grid)
+    n = len(grid[0])
+    cache = {}
+    def rec(i, j, offset):
+        if offset == len(S):
+            return True
 
+        if i < 0 or i >= m or j < 0 or j >= n:
+            return False
+
+        if (i, j, offset) not in cache:
+            cache[(i, j, offset)] = (grid[i][j] == S[offset]
+                                     and any([rec(i + 1, j, offset + 1),
+                                              rec(i - 1, j, offset + 1),
+                                              rec(i, j + 1, offset + 1),
+                                              rec(i, j - 1, offset + 1)]))
+        return cache[(i, j, offset)]
+
+
+    return any(rec(i, j, 0) for i in range(m) for j in range(n))
 
 if __name__ == '__main__':
     exit(
