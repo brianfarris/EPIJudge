9d5b9373db795135a3fe77bf219dab566d1ccec3
bfarris8 <bfarris8@bloomberg.net>
2018-09-04 10:38:30 -0400

    save

--- epi_judge_python/string_transformability.py
+++ epi_judge_python/string_transformability.py
@@ -1,10 +1,24 @@
 from test_framework import generic_test
-
+from collections import deque
+import string
 
 # Uses BFS to find the least steps of transformation.
 def transform_string(D, s, t):
     # TODO - you fill in here.
-    return 0
+    q = deque([(s, 0)])
+    D.remove(s)
+    while q:
+        vert, vert_dist = q.pop()
+        if vert == t:
+            return vert_dist
+
+        for i in range(len(vert)):
+            for char in string.ascii_lowercase:
+                candidate = vert[:i] + char + vert[i+1:]
+                if candidate in D:
+                    q.appendleft((candidate, vert_dist + 1))
+                    D.remove(candidate)
+    return -1
 
 
 if __name__ == '__main__':
