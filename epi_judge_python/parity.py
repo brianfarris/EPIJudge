5ff18ec91395b44bdfc0caa99a606da3cec212ea
bfarris8 <bfarris8@bloomberg.net>
2018-09-17 14:47:28 -0400

    added parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -1,9 +1,32 @@
 from test_framework import generic_test
 
 
+mask_size = 16
+mask = (1 << mask_size) - 1
+
+
+
+def fill_cache():
+    cache = {}
+    for y in range(1 << mask_size):
+
+        result = 0
+        x = y
+        while x:
+            result ^= 1
+            x &= x - 1
+        cache[y] = result
+    return cache
+
+cache = fill_cache()
+
+
 def parity(x):
     # TODO - you fill in here.
-    return 0
+    return (cache[(x >> (3 * mask_size)) & mask] ^
+            cache[(x >> (2 * mask_size)) & mask] ^
+            cache[(x >> (1 * mask_size)) & mask] ^
+            cache[(x >> (0 * mask_size)) & mask])
 
 
 if __name__ == '__main__':
