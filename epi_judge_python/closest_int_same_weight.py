80db03d5e58ac690d07c9a929fb555b5bb029847
bfarris8 <bfarris8@bloomberg.net>
2018-09-20 14:11:17 -0400

    closest_int_same_weight.py

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -3,6 +3,11 @@ from test_framework import generic_test
 
 def closest_int_same_bit_count(x):
     # TODO - you fill in here.
+    n_bits = 64
+    for i in range(n_bits-1):
+        if (x >> i) & 1 != (x >> (i + 1)) & 1:
+            return x ^ ((1 << i) | (1 << (i + 1)))
+        
     return 0
 
 
