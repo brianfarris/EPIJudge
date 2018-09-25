af3ca31670d02e6ebaf43eaa65a9329a2c4a2e2a
Brian Farris <bfarris@steinadmins-mbp.lan>
2018-09-24 23:50:42 -0400

    is_number_palindromic

--- epi_judge_python/is_number_palindromic.py
+++ epi_judge_python/is_number_palindromic.py
@@ -1,8 +1,23 @@
 from test_framework import generic_test
+import math
 
 
 def is_palindrome_number(x):
     # TODO - you fill in here.
+    if x < 0:
+        return False
+    if x == 0:
+        return True
+    n_digits = math.floor(math.log10(x)) + 1
+    mask = 10**(n_digits - 1)
+    for _ in range(n_digits // 2):
+        first_digit = x // mask
+        last_digit = x % 10
+        if first_digit != last_digit:
+            return False
+        x %= mask
+        x //= 10
+        mask //= 100
     return True
 
 
