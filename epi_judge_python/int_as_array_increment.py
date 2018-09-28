68d8b35202448128034b69fdf4b6b009eec580b7
Brian Farris <briandavidfarris@gmail.com>
2018-09-27 22:42:11 -0400

    increment

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -3,7 +3,16 @@ from test_framework import generic_test
 
 def plus_one(A):
     # TODO - you fill in here.
-    return []
+    A[-1] += 1
+    for i in reversed(range(1, len(A))):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        A[0] = 0
+        A.insert(0, 1)
+    return A
 
 
 if __name__ == '__main__':
