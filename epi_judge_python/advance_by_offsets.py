f5b4dfb27f5ded6254c48db43be5efe9cb34160a
Brian Farris <bfarris@SteinAdmins-MacBook-Pro.local>
2018-09-30 12:17:47 -0400

    advancing_array

--- epi_judge_python/advance_by_offsets.py
+++ epi_judge_python/advance_by_offsets.py
@@ -3,6 +3,12 @@ from test_framework import generic_test
 
 def can_reach_end(A):
     # TODO - you fill in here.
+    furthest_so_far = 0
+    i = 0
+    while i <= furthest_so_far and furthest_so_far < len(A) - 1:
+        furthest_so_far = max(furthest_so_far, A[i] + i)
+        i += 1
+    return furthest_so_far >= len(A) - 1
     return True
 
 
