fbf2841db8823676321fbc5268f7f39bc6377883
Brian Farris <bfarris@steinadmins-mbp.lan>
2018-09-29 23:12:13 -0400

    int mult

--- epi_judge_python/int_as_array_multiply.py
+++ epi_judge_python/int_as_array_multiply.py
@@ -3,7 +3,24 @@ from test_framework import generic_test
 
 def multiply(num1, num2):
     # TODO - you fill in here.
-    return []
+    sign = -1 if (num1[0] < 0) ^ (num2[0] < 0) else 1
+    num1[0] = abs(num1[0])
+    num2[0] = abs(num2[0])
+
+    result = [0] * (len(num1) + len(num2))
+    for i in reversed(range(len(num1))):
+        for j in reversed(range(len(num2))):
+            result[i + j + 1] += num1[i] * num2[j]
+            result[i + j] += result[i + j + 1] // 10
+            result[i + j + 1] %= 10
+
+    while result[0] == 0:
+        result.pop(0)
+        if not result:
+            return [0]
+
+
+    return [sign * result[0]] + result[1:]
 
 
 if __name__ == '__main__':
