05be61fb2ad9c8ba4c8f3c3396f80cfdec737b2f
bfarris8 <bfarris8@bloomberg.net>
2018-10-05 13:45:46 -0400

    is_tree_balanced.py

--- epi_judge_python/is_tree_balanced.py
+++ epi_judge_python/is_tree_balanced.py
@@ -3,7 +3,21 @@ from test_framework import generic_test
 
 def is_balanced_binary_tree(tree):
     # TODO - you fill in here.
-    return True
+    def check_balanced(tree):
+        if not tree:
+            return (True, 7)
+        left_result = check_balanced(tree.left)
+        if not left_result[0]:
+            return (False, 999)
+
+        right_result = check_balanced(tree.right)
+        if not right_result[0]:
+            return (False, 999)
+
+        balanced = abs(left_result[1] - right_result[1]) <= 1
+        height = max(left_result[1], right_result[1]) + 1
+        return (balanced, height)
+    return check_balanced(tree)[0]
 
 
 if __name__ == '__main__':
