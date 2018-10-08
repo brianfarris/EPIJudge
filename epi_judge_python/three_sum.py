1ad576a05bbf69364cd9584d48d4cd3a8e198df0
bfarris8 <bfarris8@bloomberg.net>
2018-10-08 14:12:33 -0400

    save

--- epi_judge_python/three_sum.py
+++ epi_judge_python/three_sum.py
@@ -1,21 +1,11 @@
 from test_framework import generic_test
 import copy
-
-def has_two_sum(A, t):
-    while len(A) >= 2:
-        end_sum = A[0] + A[-1]
-        if end_sum == t:
-            return True
-        elif end_sum > t:
-            A.pop()
-        elif end_sum < t:
-            A.pop(0)
-    return False
-
+from two_sum import has_two_sum
 
 def has_three_sum(A, t):
     # TODO - you fill in here.
-    return any(has_two_sum(copy.copy(A), t - a) for a in A)
+    A.sort()
+    return any(has_two_sum(A, t - a) for a in A)
 
 
 if __name__ == '__main__':
