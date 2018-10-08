0635192bcc80faee68f75c44597f2a8b4e1a6ec1
bfarris8 <bfarris8@bloomberg.net>
2018-10-08 13:32:21 -0400

    save

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -1,17 +1,22 @@
 from test_framework import generic_test
+import collections
 
 
-def is_binary_tree_bst(tree, low_range=float('-inf'), high_range=float('inf')):
-    if not tree:
-        return True
-    elif not low_range <= tree.data <= high_range:
-        return False
-    else:
-        return (is_binary_tree_bst(tree.left, low_range, tree.data) 
-                and is_binary_tree_bst(tree.right, tree.data, high_range))
+def is_binary_tree_bst(tree):
+    
+    q = collections.deque([[tree, float('-inf'), float('inf')]])
+
+    while q:
+        n = q.popleft()
+        if n[0]:
+            if not n[1] <= n[0].data <= n[2]:
+                return False
+            q += [[n[0].left, n[1], n[0].data], [n[0].right, n[0].data, n[2]]]
+    return True
 
 
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("is_tree_a_bst.py", 'is_tree_a_bst.tsv',
                                        is_binary_tree_bst))
+
