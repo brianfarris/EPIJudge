8d79dffdb89247f6bc4aa5799487b97eee9da4d5
Brian Farris <bfarris@steinadmins-mbp.lan>
2018-10-12 21:54:44 -0400

    apply_permutation

--- epi_judge_python/apply_permutation.py
+++ epi_judge_python/apply_permutation.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def apply_permutation(perm, A):
     # TODO - you fill in here.
-    return
+    for i in range(len(A)):
+        next = i
+        while perm[next] >= 0:
+            A[i], A[perm[next]] = A[perm[next]], A[i]
+            temp = perm[next]
+            perm[next] -= len(perm)
+            next = temp
+    perm[:] = [a + len(perm) for a in perm]
 
 
 def apply_permutation_wrapper(perm, A):
