33116c7e0cc4268f538c71295bc04a363309a4cf
bfarris8 <bfarris8@bloomberg.net>
2018-10-26 16:38:53 -0400

    convert_base

--- epi_judge_python/convert_base.py
+++ epi_judge_python/convert_base.py
@@ -1,9 +1,28 @@
 from test_framework import generic_test
+import string
 
 
 def convert_base(num_as_string, b1, b2):
     # TODO - you fill in here.
-    return ''
+    if num_as_string[0] == "-":
+        sign = "-"
+        num_as_string = num_as_string[1:]
+    else:
+        sign = ""
+
+    int_rep = 0
+    for i, digit in enumerate(reversed(num_as_string)):
+        int_digit = string.hexdigits.index(digit.lower())
+        int_rep += int_digit * b1 ** i
+
+    output = ''
+    while True:
+        output += string.hexdigits[int_rep % b2].upper()
+        int_rep //= b2
+        if int_rep == 0:
+            break
+    
+    return sign + output[::-1]
 
 
 if __name__ == '__main__':
