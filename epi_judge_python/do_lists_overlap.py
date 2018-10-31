37c311e4bea519dae4099a07d37768031175d083
Brian Farris <bfarris@SteinAdmins-MacBook-Pro.local>
2018-10-31 11:28:21 -0400

    linked_lists work

--- epi_judge_python/do_lists_overlap.py
+++ epi_judge_python/do_lists_overlap.py
@@ -3,12 +3,54 @@ import functools
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 from test_framework.test_utils import enable_executor_hook
-
+from is_list_cyclic import has_cycle
+from do_terminated_lists_overlap import overlapping_no_cycle_lists
 
 def overlapping_lists(l0, l1):
     # TODO - you fill in here.
-    return None
+    root0 = has_cycle(l0)
+    root1 = has_cycle(l1)
 
+    if not root0 and not root1:
+        return overlapping_no_cycle_lists(l0, l1)
+    elif (root0 and not root1) or (root1 and not root0):
+        return None
+
+    runner = root1
+    while True:
+        runner = runner.next
+        if runner is root0 or runner is root1:
+            break
+
+    if runner is not root0:
+        return None
+
+    def distance(a, b):
+        dis = 0
+        while a is not b:
+            a = a.next
+            dis += 1
+        return dis
+
+    stem_length0 = distance(l0, root0)
+    stem_length1 = distance(l1, root1)
+
+    # make sure that root0 comes first
+    if stem_length0 > stem_length1:
+        l0, l1 = l1, l0
+        root0, root1 = root1, root0
+
+    for _ in range(abs(stem_length0 - stem_length1)):
+        l1 = l1.next
+
+    while l0 is not l1 and l0 is not root0 and l1 is not root1:
+        l0 = l0.next
+        l1 = l1.next
+
+    if l0 is l1:
+        return l0
+    else:
+        return root1
 
 @enable_executor_hook
 def overlapping_lists_wrapper(executor, l0, l1, common, cycle0, cycle1):
--- epi_judge_python/phone_number_mnemonic.py
+++ epi_judge_python/phone_number_mnemonic.py
@@ -3,8 +3,21 @@ from test_framework import generic_test, test_utils
 
 def phone_mnemonic(phone_number):
     # TODO - you fill in here.
-    return []
+    MAPPING = (
+            '0', '1', 'ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ'
+            )
+    output = []
+    partial = [0] * len(phone_number)
+    def rec(digit):
+        if digit == len(phone_number):
+            output.append(''.join(partial))
+        else:
+            for c in MAPPING[int(phone_number[digit])]:
+                partial[digit] = c
+                rec(digit + 1)
 
+    rec(0)
+    return output
 
 if __name__ == '__main__':
     exit(
