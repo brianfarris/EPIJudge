caf4b1b09b5b3be366b851c5bc1717ae5df65b2f
bfarris8 <bfarris8@bloomberg.net>
2018-11-02 12:57:08 -0400

    remove duplicates

--- epi_judge_python/remove_duplicates_from_sorted_list.py
+++ epi_judge_python/remove_duplicates_from_sorted_list.py
@@ -3,7 +3,14 @@ from test_framework import generic_test
 
 def remove_duplicates(L):
     # TODO - you fill in here.
-    return None
+    runner1 = L
+    while runner1:
+        runner2 = runner1.next
+        while runner2 and runner2.data == runner1.data:
+            runner2 = runner2.next
+        runner1.next = runner2
+        runner1 = runner1.next
+    return L
 
 
 if __name__ == '__main__':
