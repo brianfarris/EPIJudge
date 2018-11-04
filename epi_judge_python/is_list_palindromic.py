d3aa9a40ebadb2ec4bbb81812c903aec292da038
Brian Farris <bfarris@SteinAdmins-MacBook-Pro.local>
2018-11-04 11:51:33 -0500

    list_palindromic

--- epi_judge_python/is_list_palindromic.py
+++ epi_judge_python/is_list_palindromic.py
@@ -3,8 +3,30 @@ from test_framework import generic_test
 
 def is_linked_list_a_palindrome(L):
     # TODO - you fill in here.
-    return True
+    def reverse_list(L):
+        prev = None
+        while L:
+            temp = L.next
+            L.next = prev
+            prev = L
+            L = temp
+        return prev
 
+    slow = L
+    fast = L
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    first_iter = L
+    second_iter = reverse_list(slow)
+    while first_iter and second_iter:
+        if first_iter.data != second_iter.data:
+            return False
+        else:
+            first_iter = first_iter.next
+            second_iter = second_iter.next
+    return True
 
 if __name__ == '__main__':
     exit(
