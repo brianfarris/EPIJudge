e77f1ea93d7d6f88ffa5d261adeafd5d876a04e5
bfarris8 <bfarris8@bloomberg.net>
2018-11-08 14:32:21 -0500

    rpn

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -3,7 +3,20 @@ from test_framework import generic_test
 
 def evaluate(expression):
     # TODO - you fill in here.
-    return 0
+    OPERATORS = {
+            "+": lambda y, x: x + y,
+            "-": lambda y, x: x - y,
+            "*": lambda y, x: x * y,
+            "/": lambda y, x: int(x / y)
+            }
+    stack = []
+    for token in expression.split(","):
+        if token in OPERATORS:
+            result = OPERATORS[token](stack.pop(), stack.pop())
+            stack.append(result)
+        else:
+            stack.append(int(token))
+    return stack[0]
 
 
 if __name__ == '__main__':
