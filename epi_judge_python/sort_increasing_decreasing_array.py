3c7e8bcde4bc6f9ffdf41f36c3047b41b53c1aaf
bfarris8 <bfarris8@bloomberg.net>
2018-11-27 17:21:36 -0500

    sort increasing decreasing

--- epi_judge_python/sort_increasing_decreasing_array.py
+++ epi_judge_python/sort_increasing_decreasing_array.py
@@ -3,26 +3,41 @@ from sorted_arrays_merge import merge_sorted_arrays
 
 
 def sort_k_increasing_decreasing_array(A):
-    # TODO - you fill in here.
+    sorted_subarrays = []
+    increasing = True
     start = 0
-    direction = "inc"
-    subarrays = []
-    for i in range(1, len(A)):
-        if direction == "inc" and A[i] < A[i-1]:
-            subarrays.append(A[start:i])
-            direction = "dec"
+    for i in range(1, len(A) + 1):
+        if (i == len(A) or A[i] > A[i - 1]) and not increasing:
+            sorted_subarrays.append(A[i-1:start-1:-1])
             start = i
-        elif direction == "dec" and A[i] > A[i-1]:
-            subarrays.append(list(reversed(A[start:i])))
-            direction = "inc"
+            increasing = True
+        elif (i == len(A) or A[i] < A[i - 1]) and increasing:
+            sorted_subarrays.append(A[start:i])
             start = i
-    if direction == "inc":
-        subarrays.append(A[start:])
-    else:
-        subarrays.append(list(reversed(A[start:])))
+            increasing = False
+    return merge_sorted_arrays(sorted_subarrays)
+
+# def sort_k_increasing_decreasing_array(A):
+#     # TODO - you fill in here.
+#     start = 0
+#     direction = "inc"
+#     subarrays = []
+#     for i in range(1, len(A)):
+#         if direction == "inc" and A[i] < A[i-1]:
+#             subarrays.append(A[start:i])
+#             direction = "dec"
+ #            start = i
+#         elif direction == "dec" and A[i] > A[i-1]:
+#             subarrays.append(list(reversed(A[start:i])))
+#             direction = "inc"
+#             start = i
+#     if direction == "inc":
+#         subarrays.append(A[start:])
+#     else:
+#         subarrays.append(list(reversed(A[start:])))
 
 
-    return merge_sorted_arrays(subarrays)
+#     return merge_sorted_arrays(subarrays)
 
 
 if __name__ == '__main__':
