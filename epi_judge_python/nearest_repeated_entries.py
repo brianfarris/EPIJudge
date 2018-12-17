7441400d97613ca5b072edd93ad98b9e7644c66c
Brian Farris <briandavidfarris@gmail.com>
2018-12-16 22:26:57 -0500

    nearest

--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -2,20 +2,19 @@ from test_framework import generic_test
 
 
 def find_nearest_repetition(paragraph):
-    # TODO - you fill in here.
     cache = {}
-    min_dist = float('inf')
-    i = 0
-    for word in paragraph:
+    min_dist = float("inf")
+    for i, word in enumerate(paragraph):
         if word in cache:
-            dist = i - cache[word]
-            min_dist = min(min_dist, dist)
+            i_eq = cache[word]
+            min_dist = min(min_dist, i - i_eq)
+
         cache[word] = i
-        i += 1
-    if min_dist == float('inf'):
+    if min_dist == float("inf"):
         return -1
     else:
         return min_dist
+
 
 if __name__ == '__main__':
     exit(
