9a759f5ef8464faa93396f1a6170955066176601
Brian Farris <bfarris@steinadmins-mbp.lan>
2019-01-12 21:29:45 -0500

    num score combinations

--- epi_judge_python/number_of_score_combinations.py
+++ epi_judge_python/number_of_score_combinations.py
@@ -4,6 +4,22 @@ import copy
 
 
 def num_combinations_for_final_score(final_score, individual_play_scores):
+    n_scores = len(individual_play_scores)
+
+    storage = {}
+    for use_index in range(n_scores):
+        storage[(use_index, 0)] = 1
+
+    for use_index in range(n_scores):
+        for j in range(1, final_score + 1):
+            without_this = storage[(use_index-1, j)] if use_index > 0 else 0
+            with_this = storage[(use_index, j - individual_play_scores[use_index])] if j >= individual_play_scores[use_index] else 0
+            storage[(use_index, j)] = without_this + with_this
+
+    return storage[(n_scores-1, final_score)]
+
+"""
+def num_combinations_for_final_score(final_score, individual_play_scores):
     # TODO - you fill in here.
     storage = {}
     for x in range(len(individual_play_scores)+1):
@@ -19,6 +35,9 @@ def num_combinations_for_final_score(final_score, individual_play_scores):
                                                                   this_final_score - num_uses * individual_play_scores[use_index])]
 
     return storage[(len(individual_play_scores), final_score)]
+"""
+
+
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main("number_of_score_combinations.py",
