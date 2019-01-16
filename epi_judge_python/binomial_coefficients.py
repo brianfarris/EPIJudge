20ba2e8f289a8889c5d0e242ef87a2e464f89cea
Brian Farris <bfarris@steinadmins-mbp.lan>
2019-01-15 21:59:41 -0500

    binomial

--- epi_judge_python/binomial_coefficients.py
+++ epi_judge_python/binomial_coefficients.py
@@ -3,6 +3,17 @@ from test_framework import generic_test
 
 cache = {}
 def compute_binomial_coefficient(n, k):
+    if k in (0, n):
+        return 1
+
+    if (n, k) not in cache:
+        cache[(n,k)] = (compute_binomial_coefficient(n - 1, k - 1) +
+                        compute_binomial_coefficient(n -  1, k))
+    return cache[(n, k)]
+
+"""
+cache = {}
+def compute_binomial_coefficient(n, k):
     # TODO - you fill in here.
     if k in (0, n):
         return 1
@@ -10,10 +21,10 @@ def compute_binomial_coefficient(n, k):
     if (n, k) in cache:
         return cache[(n, k)]
 
-
-
     cache[(n, k)] = compute_binomial_coefficient(n - 1, k - 1) + compute_binomial_coefficient(n - 1, k)
     return cache[(n, k)]
+"""
+
 
 if __name__ == '__main__':
     exit(
